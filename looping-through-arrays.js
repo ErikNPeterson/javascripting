557ea1ecb58dd9281f311f48b7958ed151b3dd53
Erik <erikeh1@gmail.com>
2018-09-16 18:24:51 +0000

    This is a commit of the last few exercises in Javascripting

--- looping-through-arrays.js
+++ looping-through-arrays.js
@@ -1,8 +1,7 @@
 var pets = ['cat', 'dog', 'rat'];
-/*for (var i = 0 ; i < pets.length ; i++) {
-  pets [i] = pets[i] + 's'
-};
- console.log(pets); */
- pets.forEach((javathin)=> {javathin = javathin + 's'});
+for (var i = 0 ; i < pets.length ; i++) {
+  pets [i] += 's';
+}
  console.log(pets);
+ //pets.forEach((javathin)=> {javathin = javathin + 's'});
  // forEach function is slower
